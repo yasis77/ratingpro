6fa4a83232eb8bcf7be5a7a1522a1b13c65a17f5
Yasmin Salehi <93091297+yasis77@users.noreply.github.com>
2023-06-01 20:01:51 -0600

    Add files via upload

---
+++ finalproject/src/DataAnalyzer.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+
+public abstract class DataAnalyzer {
+
+    Parser parser;
+    
+    public DataAnalyzer(Parser p) {
+        parser = p;
+        extractInformation();
+    }
+
+    public abstract MyHashTable<String, Integer> getDistByKeyword(String keyword);
+
+    public abstract void extractInformation();
+  
+    
+}
---
+++ finalproject/src/RatingByGender.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+
+public class RatingByGender extends DataAnalyzer{
+
+	public RatingByGender(Parser p) {
+		super(p);
+	}
+
+	@Override
+	public MyHashTable<String, Integer> getDistByKeyword(String keyword) {
+		// ADD YOUR CODE BELOW THIS
+		return null;
+		//ADD YOUR CODE ABOVE THIS
+	}
+
+	@Override
+	public void extractInformation() {
+		// ADD YOUR CODE BELOW THIS
+
+		//ADD YOUR CODE ABOVE THIS
+	}
+}
